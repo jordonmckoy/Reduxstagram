01a180386d0fd21169f9b04f291281d7791b580e
Jordon McKoy <jordonmckoy@gmail.com>
2016-05-16 19:09:20 -0400

    added redux store and router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+	render() {
+		return (
+			<div className="photo-grid">
+				Photo..
+			</div>
+		)
+	}
+});
+
+export default PhotoGrid;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // Import css
 import css from './styles/style.styl';
 
-render(<p>hi</p>, document.getElementById('root'));
+// Import components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={PhotoGrid}></IndexRoute>
+			<Route path="/view/:postId" component={Single}></Route>
+		</Route>
+	</Router>
+)
+
+render(router, document.getElementById('root'));
